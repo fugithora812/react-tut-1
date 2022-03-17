8c7f0769a5c7ab810518827e2e5fd94d75031e2f
ivis-kuwata <jumpei.kuwata@ivis.co.jp>
2022-03-17 14:59:53 -0700

    update: tutorial

--- src/index.js
+++ src/index.js
@@ -2,24 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => this.props.onClick}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
 
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.setState({ value: 'X' })}
-      >
-        {this.state.value}
-      </button>
-    );
-  }
+
+function Square(props) {
+  return (
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
@@ -27,7 +29,18 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    squares[i] = "X";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
